8b4d9c4d16973b86de35cfcb63ec511aefa58349
Valentino Filipetto <valefilipetto@gmail.com>
2023-11-07 16:43:38 +0100

    feat: handle markdown & improve styling/font

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const { DateTime } = require("luxon");
+const eleventyGoogleFonts = require("eleventy-google-fonts");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPlugin(eleventyGoogleFonts);
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime);
+  });
+
   return {
     dir: {
       input: "src",

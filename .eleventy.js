36b7ddcd88209f7c6725a059eab064277ae04721
Valentino Filipetto <valefilipetto@gmail.com>
2024-05-04 21:53:06 +0200

    style: update article styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,15 @@
 const { DateTime } = require("luxon");
 const eleventyGoogleFonts = require("eleventy-google-fonts");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/style");
   eleventyConfig.addPassthroughCopy("./public/img");
   eleventyConfig.addPlugin(eleventyGoogleFonts);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    preAttributes: { tabindex: 0 },
+  });
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime);

0c6ee701710a26c43e39a160e48f26f7f01a4f3a
Valentino Filipetto <valefilipetto@gmail.com>
2024-05-16 15:01:21 +0200

    style: improve article styling and especially quotations

--- .eleventy.js
+++ .eleventy.js
@@ -2,16 +2,14 @@ const { DateTime } = require("luxon");
 const eleventyGoogleFonts = require("eleventy-google-fonts");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/style.css");
-  eleventyConfig.addPassthroughCopy("./src/style");
-  eleventyConfig.addPassthroughCopy("./public/img");
-  eleventyConfig.addPlugin(eleventyGoogleFonts);
-  eleventyConfig.addPlugin(syntaxHighlight, {
-    preAttributes: { tabindex: 0 },
-  });
+module.exports = function (config) {
+  config.addPassthroughCopy("./src/style.css");
+  config.addPassthroughCopy("./src/style");
+  config.addPassthroughCopy("./public/img");
+  config.addPlugin(eleventyGoogleFonts);
+  config.addPlugin(syntaxHighlight);
 
-  eleventyConfig.addFilter("postDate", (dateObj) => {
+  config.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime);
   });
 
--- src/writing/first-article.md
+++
@@ -1,8 +0,0 @@
----
-layout: article.njk
-title: On Mahler's Sixth symphony in three pictures
-author: Valentino Filipetto
-date: 2023-06-11
-tags: post
-description: Claudio's Abbado facial expressions
----
